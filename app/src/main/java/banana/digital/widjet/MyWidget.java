bf9fa9395473142d191d69aa9a8daa9a9419bf22
Banandroid Z <ndrbnnz@gmail.com>
2018-11-19 20:58:21 +0300

    widget 1.1

--- app/src/main/java/banana/digital/widjet/MyWidget.java
+++ app/src/main/java/banana/digital/widjet/MyWidget.java
@@ -1,8 +1,13 @@
 package banana.digital.widjet;
 
+import android.app.AlarmManager;
+import android.app.AppOpsManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.widget.RemoteViews;
 
 import java.util.Calendar;
@@ -10,6 +15,9 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class MyWidget extends AppWidgetProvider {
+
+    private static final String REFRESH_ACTION = "REFRESH_ACTION";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
@@ -28,5 +36,32 @@ public class MyWidget extends AppWidgetProvider {
                     + " : " + minstr);
             appWidgetManager.updateAppWidget(currentId, views);
         }
+        scheduleUpdates(context, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction() != null && intent.getAction().equals(REFRESH_ACTION)) {
+            final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            final int[] appWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_ID);
+            onUpdate(context, appWidgetManager, appWidgetIds);
+        }
+    }
+
+    private void scheduleUpdates(Context context, int[] appWidgetIds) {
+        final AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        final Intent intent = new Intent(context, MyWidget.class);
+        intent.setAction(REFRESH_ACTION);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds);
+        final PendingIntent pendingIntent= PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis() + 60 * 1000);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.set(Calendar.SECOND, 0);
+        final long time = calendar.getTimeInMillis();
+
+        alarmManager.setExact(AlarmManager.RTC, time, pendingIntent);
     }
 }
